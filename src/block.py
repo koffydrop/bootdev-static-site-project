4ec28ca77cda49506938c0eecd136a6f1d21a1f6
kira <67888720+koffydrop@users.noreply.github.com>
2025-05-27 23:25:55 -0300

    implement block types and tests

--- src/block.py
+++ src/block.py
@@ -1,3 +1,15 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
 def markdown_to_blocks(md: str) -> list[str]:
     blocks = list(
         filter(
@@ -6,3 +18,20 @@ def markdown_to_blocks(md: str) -> list[str]:
         )
     )
     return blocks
+
+
+def block_to_block_type(block: str) -> BlockType:
+    lines = block.split("\n")
+    if any(block.startswith(h) for h in ["#" * i + " " for i in range(1, 7)]):
+        return BlockType.HEADING
+    if block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    if all(line.startswith(">") for line in lines):
+        return BlockType.QUOTE
+    if all(line.startswith("- ") for line in lines):
+        return BlockType.UNORDERED_LIST
+    if block.startswith("1.") and all(
+        lines[i].startswith(str(i + 1)) for i in range(len(lines))
+    ):
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -48,3 +48,53 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_block_to_heading(self):
+        blocks = [
+            "not a heading",
+            "# A heading",
+            "## Another",
+            "### Three",
+            "#### Four",
+            "##### Now number five",
+            "###### I never wanna hear you say",
+            "####### im just a paragraph",
+        ]
+        result = list(map(b.block_to_block_type, blocks))
+        expect = [
+            b.BlockType.PARAGRAPH,
+            b.BlockType.HEADING,
+            b.BlockType.HEADING,
+            b.BlockType.HEADING,
+            b.BlockType.HEADING,
+            b.BlockType.HEADING,
+            b.BlockType.HEADING,
+            b.BlockType.PARAGRAPH,
+        ]
+        self.assertListEqual(result, expect)
+
+    def test_block_to_code(self):
+        block = """```rust
+fn main() {
+    println!("Hi there");
+}
+```"""
+        self.assertEqual(b.block_to_block_type(block), b.BlockType.CODE)
+
+    def test_block_to_quote(self):
+        block = """> something really smart
+> said by someone really smart"""
+        self.assertEqual(b.block_to_block_type(block), b.BlockType.QUOTE)
+
+    def test_block_to_unordered_list(self):
+        block = """- an item
+- another
+- and another"""
+        self.assertEqual(b.block_to_block_type(block), b.BlockType.UNORDERED_LIST)
+
+    def test_block_to_ordered_list(self):
+        block = """1. one
+2. two
+3. three
+4. four"""
+        self.assertEqual(b.block_to_block_type(block), b.BlockType.ORDERED_LIST)
